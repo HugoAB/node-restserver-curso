4b8e80f44c226f6ba6f70b931719f789718d3d89
HugoAB <hugoabobadillav@outlook.com>
2020-11-22 00:26:37 -0300

    REST server-usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,22 @@
 // Puerto
 // ===================
 process.env.PORT = process.env.PORT || 3000;
+
+// ===================
+// Entorno
+// ===================
+process.env.NODE_ENV = process.env.NODE_ENV  || 'dev';
+
+
+// ===================
+// Base de datos
+// ===================
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://hugo:Temporal2020*@cluster0.ehcc9.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
