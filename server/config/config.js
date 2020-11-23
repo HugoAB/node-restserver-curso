07edeb37706f68d4c2bdaa9307675c68e83b49c9
HugoAB <hugoabobadillav@outlook.com>
2020-11-22 22:14:46 -0300

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://hugo:Temporal2020*@cluster0.ehcc9.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
